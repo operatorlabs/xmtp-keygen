50dd3c6c9e637605ff54d6f430242f9b72aded16
Kevin Joo <kevinj507@gmail.com>
2024-01-15 20:15:45 -0500

    state changes working

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,148 +1,66 @@
-// import { Client } from "@xmtp/xmtp-js";
-// import { Wallet, ethers } from "ethers";
-// import React, { useEffect, useState, useRef } from "react";
-// import Chat from "./Chat";
-// import styles from "./Home.module.css";
-
-// const PEER_ADDRESS = "0xf499f581c805047f8cefd5575d4f940492f26cb1";
-
-// export default function Home() {
-//   const [messages, setMessages] = useState(null);
-//   const convRef = useRef(null);
-//   const clientRef = useRef(null);
-//   const [signer, setSigner] = useState(null);
-//   const [isConnected, setIsConnected] = useState(false);
-//   const [isOnNetwork, setIsOnNetwork] = useState(false);
-
-//   // Function to load the existing messages in a conversation
-//   const newConversation = async function (xmtp_client, addressTo) {
-//     //Creates a new conversation with the address
-//     if (await xmtp_client?.canMessage(addressTo)) {
-//       const conversation = await xmtp_client.conversations.newConversation(
-//         addressTo,
-//       );
-//       convRef.current = conversation;
-//       const messages = await conversation.messages();
-//       setMessages(messages);
-//     } else {
-//       console.log("cant message because is not on the network.");
-//       //cant message because is not on the network.
-//     }
-//   };
-
-//   // Function to initialize the XMTP client
-//   const initXmtp = async function () {
-//     // Create the XMTP client
-//     const xmtp = await Client.create(signer, { env: "production" });
-
-//     // Cache the public XMTP key
-//     const keys = await Client.getKeys(signer, {skipContactPublishing: true, persistConversations: false});
-//     // const publicKey = keyBundle.publicKey;
-//     // const privateKey = keyBundle.privateKey;
-//     console.log(`KEYS: ${JSON.stringify(keys)}`);
-//     // console.log(`PUBLIC: ${publicKey}`)
-//     // console.log(`PRIVATE: ${privateKey}`)
-
-//     //Create or load conversation with Gm bot
-//     newConversation(xmtp, PEER_ADDRESS);
-//     // Set the XMTP client in state for later use
-//     setIsOnNetwork(!!xmtp.address);
-//     //Set the client in the ref
-//     clientRef.current = xmtp;
-//   };
-
-//   // Function to connect to the wallet
-//   const connectWallet = async function () {
-//     // Check if the ethereum object exists on the window object
-//     if (typeof window.ethereum !== "undefined") {
-//       try {
-//         // Request access to the user's Ethereum accounts
-//         await window.ethereum.enable();
-
-//         // Instantiate a new ethers provider with Metamask
-//         const provider = new ethers.providers.Web3Provider(window.ethereum);
-
-//         // Get the signer from the ethers provider
-//         const signer = provider.getSigner()
-//         setSigner(signer);
-//         const addr = await signer.getAddress();
-//         console.log(`Signer: ${addr}`)
-
-//         // Update the isConnected data property based on whether we have a signer
-//         setIsConnected(true);
-//       } catch (error) {
-//         console.error("User rejected request", error);
-//       }
-//     } else {
-//       console.error("Metamask not found");
-//     }
-//   };
-//   useEffect(() => {
-//     if (isOnNetwork && convRef.current) {
-//       // Function to stream new messages in the conversation
-//       const streamMessages = async () => {
-//         const newStream = await convRef.current.streamMessages();
-//         for await (const msg of newStream) {
-//           const exists = messages.find((m) => m.id === msg.id);
-//           if (!exists) {
-//             setMessages((prevMessages) => {
-//               const msgsnew = [...prevMessages, msg];
-//               return msgsnew;
-//             });
-//           }
-//         }
-//       };
-//       streamMessages();
-//     }
-//   }, [messages, isConnected, isOnNetwork]);
-
-//   return (
-//     <div className={styles.Home}>
-//       {/* Display the ConnectWallet component if not connected */}
-//       {!isConnected && (
-//         <div className={styles.walletBtn}>
-//           <button onClick={connectWallet} className={styles.btnXmtp}>
-//             Connect Wallet
-//           </button>
-//         </div>
-//       )}
-//       {/* Display XMTP connection options if connected but not initialized */}
-//       {isConnected && !isOnNetwork && (
-//         <div className={styles.xmtp}>
-//           {signer?.address}
-//           <button onClick={initXmtp} className={styles.btnXmtp}>
-//             Connect to XMTP
-//           </button>
-//         </div>
-//       )}
-//       {/* Render the Chat component if connected, initialized, and messages exist */}
-//       {isConnected && isOnNetwork && messages && (
-//         <Chat
-//           client={clientRef.current}
-//           conversation={convRef.current}
-//           messageHistory={messages}
-//         />
-//       )}
-//     </div>
-//   );
-// }
-
 import { Client } from "@xmtp/xmtp-js";
 import { ethers } from "ethers";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Home.module.css"; // Import the CSS module
 
 export default function Home() {
   const [signer, setSigner] = useState(null);
   const [isConnected, setIsConnected] = useState(false);
+  const [keysDownloaded, setKeysDownloaded] = useState(false);
+  const [walletAddress, setWalletAddress] = useState(null);
 
-  // Function to connect to the wallet
+  function DownloadMessage({ walletAddress }) {
+    return (
+      <div className="contentContainer">
+        <div className={styles.message}>
+          <p className={styles.centerText}>
+            Success!
+            <br></br>
+            <br></br>
+            Check your Downloads folder for a file with {walletAddress} in the name.
+          </p>
+          <br />
+          <p className={styles.centerText}>
+            You can create a XMTP client from this file by adapting the code
+            below. 
+            <br></br>
+            Please keep your key bundle secure by not exposing it unnecessarily.
+          </p>
+          <pre className={styles.codeBlock}>
+            <code>
+              {`
+  import fs from 'fs';
+  import { Client } from '@xmtp/xmtp-js'
+  const keyBundlePath = 'path to your key bundle you downloaded'
+  if (fs.existsSync(keyBundlePath)) {
+    const keyBundleBinary = fs.readFileSync(keyBundlePath, 'utf-8');
+    // Convert the binary string back into a Buffer
+    const keyBundle = Buffer.from(keyBundleBinary, 'binary');
+    const client = await Client.create(null, {
+      env: "dev", // choose xmtp network here
+      privateKeyOverride: keyBundle
+    });
+              `}
+            </code>
+          </pre>
+          <br></br>
+          <br></br>
+          <p className={styles.centerText}>
+            To re-connect your wallet or connect a different wallet, refresh
+            this page.
+          </p>
+        </div>
+      </div>
+    );
+  }
+
+  // Connect the user's wallet
   const connectWallet = async function () {
     if (typeof window.ethereum !== "undefined") {
       try {
         await window.ethereum.enable();
         const provider = new ethers.providers.Web3Provider(window.ethereum);
-        const signer = provider.getSigner()
+        const signer = provider.getSigner();
         setSigner(signer);
         setIsConnected(true);
       } catch (error) {
@@ -153,38 +71,92 @@ export default function Home() {
     }
   };
 
-  // Function to generate the XMTP key bundle
+  // Use the connected wallet's signer to generate XMTP key bundle
   const generateKeys = async function () {
     if (signer) {
-      const keys = await Client.getKeys(signer, {skipContactPublishing: true, persistConversations: false});
-      const address = await signer.getAddress();     
-       downloadKeys(address, keys);
+      const keys = await Client.getKeys(signer, {
+        skipContactPublishing: true,
+        persistConversations: false,
+      });
+      const address = await signer.getAddress();
+      setWalletAddress(address);
+      downloadKeys(address, keys);
     }
   };
 
-  // Function to download the XMTP key bundle
+  // Convert the key bundle into text file that can be loaded later
   const downloadKeys = function (walletAddress, keys) {
     const ENCODING = "binary";
     const a = document.createElement("a");
-    const data = new Blob([Buffer.from(keys).toString(ENCODING)], {type: "text/plain"});
+    const data = new Blob([Buffer.from(keys).toString(ENCODING)], {
+      type: "text/plain",
+    });
     a.href = URL.createObjectURL(data);
     a.download = `${walletAddress}_XMTPbundle.txt`;
     a.click();
+    setKeysDownloaded(true);
   };
 
+  //   return (
+  //     <div className={styles.Home}>
+  //       {!isConnected && (
+  //         <div className={styles.walletBtn}>
+  //           <button onClick={connectWallet} className={styles.btnXmtp}>
+  //             Connect Wallet
+  //           </button>
+  //         </div>
+  //       )}
+  //       {isConnected && (
+  //         <div className={styles.walletBtn}>
+  //           <button onClick={generateKeys} className={styles.btnXmtp}>
+  //             Generate and Download XMTP Key Bundle
+  //           </button>
+  //         </div>
+  //       )}
+  //       {keysDownloaded && (
+  //         <div className="contentContainer">
+  //           <div className={styles.message}>
+  //             Check your Downloads folder for a file with {walletAddress} in the name.
+  //             <br />
+  //             You can create a XMTP client from this file by adapting the code below. Please keep your key bundle secure.
+  //             <pre className={styles.codeBlock}>
+  //               <code>
+  //                 {`
+  // import fs from 'fs';
+  // import { Client } from '@xmtp/xmtp-js'
+  // const keyBundlePath = 'path to your key bundle you downloaded'
+  // if (fs.existsSync(keyBundlePath)) {
+  //   const keyBundleBinary = fs.readFileSync(keyBundlePath, 'utf-8');
+  //   // Convert the binary string back into a Buffer
+  //   const keyBundle = Buffer.from(keyBundleBinary, 'binary');
+  //   const client = await Client.create(null, {
+  //     env: "dev", // choose xmtp network here
+  //     privateKeyOverride: keyBundle
+  //   });
+  //             `}
+  //               </code>
+  //             </pre>
+  //           </div>
+  //         </div>
+  //       )}
+  //     </div>
+  //   );
   return (
-    <div className={styles.Home}> {/* Apply the CSS class to the div */}
-      {!isConnected && (
-        <div className={styles.walletBtn}> {/* Apply the CSS class to the div */}
-          <button onClick={connectWallet} className={styles.btnXmtp}> {/* Apply the CSS class to the button */}
-            Connect Wallet
-          </button>
+    <div className={styles.Home}>
+      {!keysDownloaded ? (
+        <div className={styles.walletBtn}>
+          {!isConnected ? (
+            <button onClick={connectWallet} className={styles.btnXmtp}>
+              Connect Wallet
+            </button>
+          ) : (
+            <button onClick={generateKeys} className={styles.btnXmtp}>
+              Generate and Download XMTP Key Bundle
+            </button>
+          )}
         </div>
-      )}
-      {isConnected && (
-        <button onClick={generateKeys} className={styles.btnXmtp}> {/* Apply the CSS class to the button */}
-          Generate and Download XMTP Key Bundle
-        </button>
+      ) : (
+        <DownloadMessage walletAddress={walletAddress} />
       )}
     </div>
   );
